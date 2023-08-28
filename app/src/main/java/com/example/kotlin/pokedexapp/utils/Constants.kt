e4fcfdbb3dd8fbca44f24119c1434d07bb3c6475
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-08-28 12:32:57 -0600

    Splash and Main screen binding

--- app/src/main/java/com/example/kotlin/pokedexapp/utils/Constants.kt
+++ app/src/main/java/com/example/kotlin/pokedexapp/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin.pokedexapp.utils
 
 object Constants {
+    const val SPLASHSCREEN_DURATION = 3000L
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val MAX_POKEMON_NUMBER = 1279
 }
